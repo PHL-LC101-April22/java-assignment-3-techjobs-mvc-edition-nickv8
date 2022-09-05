131d51381e7100dafab7e34f8ef90167d859187e
Nick Villone <n.villone8@gmail.com>
2022-09-05 09:37:59 -0400

    search works, but failing 3 tests

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,10 +1,15 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -24,10 +29,20 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-    @PostMapping(value="")
-    public String displaySearchResults(Model model, String searchTerm, Boolean searchType ) {
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+        ArrayList<Job> jobs;
+        model.addAttribute("searchTerm", "searchTerm");
+        model.addAttribute("searchType", "searchType");
 
-        return "search/results";
+            if (searchTerm.equals("all") || searchTerm.equals("")) {
+                jobs = JobData.findAll();
+            } else {
+                jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            }
+        model.addAttribute("jobs", jobs);
+        return "search";
+
     }
 
 }
